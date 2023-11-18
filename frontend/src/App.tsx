974109fd8c2d9e2cd9f1ae389191d876fd3d1c54
luiz-chiq <chiquetanoluiz@gmail.com>
2023-11-18 02:10:59 -0300

    feat(App): use get/planets and show all saved planets

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import AddPlanetComponent from './components/AddPlanet';
+import Card from './components/Card'
+import { useEffect, useState } from 'react';
+import SearchPlanetComponent from './components/SearchPlanet';
 
 function App() {
+
+  const [planets, setPlanets] = useState<Planet[]>([]);
+
+  interface Planet {
+    name: string;
+  }
+
+  useEffect(() => {
+    fetchPlanets();
+  }, []);
+
+  const fetchPlanets = async () => {
+    try {
+      const response = await axios.get('http://localhost:3004/planets');
+      setPlanets(response.data);
+      console.log("a")
+    } catch (error) {
+      console.error('Erro ao buscar planetas:', error);
+    }
+  };
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+    {planets.map((planet, index) => (
+        <Card key={index} title={planet.name}/>
+      ))}
+    </>
   );
 }
 
