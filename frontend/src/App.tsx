b8c1de47a263772546b5d99cca746826a6362e69
luiz-chiq <chiquetanoluiz@gmail.com>
2023-11-18 02:13:43 -0300

    feat(App): create delete planet function

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -26,12 +26,28 @@ function App() {
       console.error('Erro ao buscar planetas:', error);
     }
   };
-
+  
+  const handleDeletePlanet = async (title: string) => {
+    try {
+      const response = await axios.delete(`http://localhost:3004/planet/${title}`);
+      if (response.data.message === 'Planeta removido com sucesso') {
+        console.log('Planeta removido com sucesso:', response.data);
+        alert('Planeta removido!');
+      } else {
+        console.error('Erro ao remover planeta:', response.data.error);
+        alert('Erro ao remover planeta!');
+      }
+    } catch (error) {
+      console.error('Erro na requisição POST:', error);
+      alert('Erro na requisição POST!');
+    }
+    fetchPlanets();
+  };
   
   return (
   <>
     {planets.map((planet, index) => (
-        <Card key={index} title={planet.name}/>
+        <Card key={index} title={planet.name} onDeletePlanet={handleDeletePlanet} />
       ))}
     </>
   );
